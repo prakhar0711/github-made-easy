2ab8e8e9b31c1c13d7f32acf0601ea0b8dba2277
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-04-19 12:16:43 +0530

    search functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ExplorePage from "./pages/ExplorePage";
 import LikesPage from "./pages/LikesPage";
 import SideBar from "./components/SideBar";
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
@@ -19,6 +20,7 @@ function App() {
             <Route path="/explore" element={<ExplorePage />} />
             <Route path="/likes" element={<LikesPage />} />
           </Routes>
+          <Toaster />
         </div>
       </div>
     </>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,16 +4,60 @@ import SortRepos from "../components/SortRepos";
 import Repos from "../components/Repos";
 import Spinner from "../components/Spinner";
 import Profile from "../components/Profile";
+import { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function HomePage() {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("forks");
+
+  const getUserProfileAndRepos = useCallback(
+    async (username = "prakhar0711") => {
+      setLoading(true);
+      try {
+        const userResponse = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        const userProfile = await userResponse.json();
+        setUserProfile(userProfile);
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        setRepos(repos);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
   return (
     <div className="m-4">
-      <Search />
+      <Search onSearch={onSearch} />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <Profile />
-        <Repos />
-        <Spinner />
+        {userProfile && !loading && <Profile userProfile={userProfile} />}
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
